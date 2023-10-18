4521dde6dffa34bb855902d284339ef1d8b3b66e
Shun <tetuzin2525@gmail.com>
2023-10-18 22:36:11 +0900

    [add] 時間経過で増加

--- MagicClicker/Assets/Scripts/ClickerSceneManager.cs
+++ MagicClicker/Assets/Scripts/ClickerSceneManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using ShunLib.Manager.CommonScene;
+using System.Drawing;
 
 namespace MagicClicker.Manager
 {
@@ -18,20 +19,70 @@ namespace MagicClicker.Manager
         // ---------- クラス変数宣言 ----------
         // ---------- インスタンス変数宣言 ----------
 
+        // ゲーム時間
+        private float _gameTime = 10000f;
+        // 経過時間
+        private float _progressTime = 0f;
+        // ゲーム中フラグ
+        private bool _isPlay = false;
         // ポイント値
         private int _point = 0;
         // 1クリックで増える値
-        private int _addValue = 1;
+        private int _addClickValue = 20;
+        // 時間経過で増える値
+        private int _addProgressValue = 1;
 
         // ---------- Unity組込関数 ----------
+
+        void FixedUpdate()
+        {
+            // ゲーム中処理
+            if (_isPlay)
+            {
+                _progressTime += Time.deltaTime;
+                ProgressEvent();
+                if (_progressTime >= _gameTime)
+                {
+                    // ゲーム終了
+                    EndGame();
+                }
+            }
+        }
+
         // ---------- Public関数 ----------
         // ---------- Private関数 ----------
+
+        // ゲーム開始
+        private void StartGame()
+        {
+
+        }
+
+        // ゲーム終了
+        private void EndGame()
+        {
+
+        }
+
+        // ポイント増加
+        private void AddPoint(int point = 0)
+        {
+            _point += point;
+            _uiManager.SetText(POINT_TEXT, _point.ToString());
+        }
+
+        // 時間経過処理
+        private void ProgressEvent()
+        {
+            // TODO 仮実装
+            AddPoint(_addProgressValue);
+        }
 
         // クリッカーボタンの処理
         private void OnClickClickerBtn()
         {
-            _point += _addValue;
-            _uiManager.SetText(POINT_TEXT, _point.ToString());
+            if (!_isPlay) return;
+            AddPoint(_addClickValue);
         }
 
         // ---------- protected関数 ---------
@@ -41,6 +92,14 @@ namespace MagicClicker.Manager
         {
             // クリッカーボタンの処理設定
             _uiManager.SetButtonEvent(CLICKER_BUTTON, OnClickClickerBtn);
+        }
+
+        // イベントの設定
+        protected override void InitEvent()
+        {
+            // TODO 仮実装
+            // 時間経過でポイント増加
+            _isPlay = true;
         }
 
         // ---------- デバッグ用関数 ---------
